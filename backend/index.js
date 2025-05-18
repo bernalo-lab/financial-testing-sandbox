0ab43bba10476b8234bf46f369db2b7f5d2dc2fe
bernalo-lab <bsogeke@bernalo.com>
2025-05-18 08:52:07 +0100

    Add App Insights integration to backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,14 @@
-console.log("Backend service running");
+const appInsights = require("applicationinsights");
+appInsights.setup(process.env.APPINSIGHTS_CONNECTION_STRING).start();
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Backend is running and monitored by App Insights.\n");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
