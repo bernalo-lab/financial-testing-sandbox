a553bc24dcf1b93da2c20b5b11fbbc7e45348d9e
bernalo-lab <bsogeke@bernalo.com>
2025-05-18 16:11:32 +0100

    Successful local build with App Insights env

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Application Insights
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+
+const appInsights = new ApplicationInsights({
+  config: {
+    connectionString: process.env.REACT_APP_APPINSIGHTS_CONNECTION_STRING,
+    enableAutoRouteTracking: true
+  }
+});
+appInsights.loadAppInsights();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +21,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: log web vitals
 reportWebVitals();
