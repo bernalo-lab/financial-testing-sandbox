efcc2d9b49409c509e465aec510949eb91650a37
bernalo-lab <bsogeke@bernalo.com>
2025-05-25 16:36:04 +0100

    backend - index.js Updated 25/05 16_35

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 const SECRET_KEY = process.env.JWT_SECRET || 'sandbox-secret-key';
-const MONGO_URI = process.env.MONGO_URI || 'your-cosmos-db-connection-string';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://sandbox-cosmos-db:VHU...@sandbox-cosmos-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb';
 const DB_NAME = 'sandboxDB';
 const COLLECTION_NAME = 'users';
 
