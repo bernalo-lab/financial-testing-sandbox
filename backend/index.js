d6b3906683b0659cfa62c3f100c0238e246bbb8d
bernalo-lab <bsogeke@bernalo.com>
2025-05-25 17:22:10 +0100

    Backend - New index.js 25/05 17_22

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: '.env.example' });
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { MongoClient } = require('mongodb');
